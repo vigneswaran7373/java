678e472584265d7f19266cb18a95d73aad1199fb
vigneswaran7373 <vigneswaran7373@gmail.com>
2025-03-01 13:36:16 +0530

    Add files via upload

--- vw.java
+++ vw.java
@@ -5,14 +5,19 @@ class vw{
         int a=5;
         int b=5;
         System.out.println(a+b);
-        Scanner va=new Scanner(System.in);
+        Scanner va=new Scanner(System.in);//user input get this function
         System.out.println("enter the 2 names");
-        String name1=va.nextLine();
-        String name2=va.nextLine();
-        String name3=new String("vw");
-        String name4=new String("vw");
-        System.out.println(name1.equals(name2));
+        String name1=va.nextLine();//normal variable incase same name create one reference memory 
+        String name2=va.nextLine();//normal variable incase same name create one reference memory 
+        String name3=new String("vw"); //object string crate new raference memory each object string
+        String name4=new String("vw"); //object string crate new raference memory each object string
+        System.out.println(name1.equals(name2));//equal inbuilt function
         System.out.println(name1==name2);
         System.out.println(name3==name4);
     }
 }
+
+
+
+// hint !!!
+// java string comparision campare referance memory not string values or inside content  ok dont forget
